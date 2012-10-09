19952a6e74b2dd0a4e425f78dc408dd7635dffee
Jin Shao <shaojinvivian@gmail.com>
2012-10-09 11:04:33 +0800

    add renderer for metric interval

--- src/main/webapp/app/view/ModelDef/MetricUpdateWindow.js
+++ src/main/webapp/app/view/ModelDef/MetricUpdateWindow.js
@@ -1,92 +1,90 @@
 Ext.define('PaaSMonitor.view.ModelDef.MetricUpdateWindow', {
-    extend: 'Ext.window.Window',
-    alias: 'widget.templatewindow',
-    
-    requires: [
-               'PaaSMonitor.store.MetricStore'
-    ],
-    
-    id: 'MetricUpdateWindow',
-   
-    layout: 'fit',
-    height: 250,
-    width: 400,
-    title: '显示该资源的所有可用监测参数',
-    
-    closeAction: 'hide',
-    
-    //除窗口外背景全部变为暗色，然后其他区域不能操作
-    modal: true,
+	extend : 'Ext.window.Window',
+	alias : 'widget.templatewindow',
 
-    initComponent: function() {
-        var me = this;
-        
-        //在此处添加需要在window中使用的组件，如grid  
-        
-        var checkColumn = new Ext.ux.CheckColumn({
-		   header: 'Enabled',
-		   dataIndex: 'enabled',
-		   id: 'check',
-		   width: 55
+	requires : ['PaaSMonitor.store.MetricStore'],
+
+	id : 'MetricUpdateWindow',
+
+	layout : 'fit',
+	height : 250,
+	width : 400,
+	title : '显示该资源的所有可用监测参数',
+
+	closeAction : 'hide',
+
+	//除窗口外背景全部变为暗色，然后其他区域不能操作
+	modal : true,
+
+	initComponent : function() {
+		var me = this;
+
+		//在此处添加需要在window中使用的组件，如grid
+
+		var checkColumn = new Ext.ux.CheckColumn({
+			header : 'Enabled',
+			dataIndex : 'enabled',
+			id : 'check',
+			width : 55
 		});
-        
-        var grid = Ext.create('Ext.grid.Panel', {
-        	store: 'MetricStore',
-			columns:[checkColumn, {
-				header: '名称',  
-				dataIndex: 'templateName' 
-				},{
-				header: '间隔/ms', dataIndex: 'interval', 
-				format: '0,000',
-				flex: 1,
-				editor: {
-					xtype: 'numberfield',
-					allowblank: false
-					}
-				},{
-				header: '单位', dataIndex: 'templateUnits' 
+
+		var grid = Ext.create('Ext.grid.Panel', {
+			store : 'MetricStore',
+			columns : [checkColumn, {
+				header : '名称',
+				dataIndex : 'templateName'
+			}, {
+				header : '间隔/min',
+				dataIndex : 'interval',
+				format : '0,000',
+				flex : 1,
+				editor : {
+					xtype : 'numberfield',
+					allowblank : false
+				},
+				renderer : function(value, cellmeta, record, rowIndex, columnIndex, store) {
+					var readable = value /(1000*60);
+					return readable;
+				}
+			}, {
+				header : '单位',
+				dataIndex : 'templateUnits'
 			}],
-			
-			height: 200,
-			width: 400,
-			
-			plugins:[
-				Ext.create('Ext.grid.plugin.CellEditing', {
-					clicksToEdit: 1
-				})
-			],
-			
-			dockedItems: [{
-				xtype: 'pagingtoolbar',
-				store: 'MetricStore',
-				dock: 'bottom',
-				displayInfo: true
-    		}],
-        });
-        
-        me.items = [grid];
-        
-        var _store = grid.getStore();
-        //var _selModel = grid.getSelectionModel();
-        
-      
-        
-        var ok = Ext.create('Ext.Button', {
-        	id: 'update_metrics_button',
-        	text: '生成Metric',        	
-        });
-        
-        var cancel = Ext.create('Ext.Button', {
-        	text: '取消', 
-        	handler: function() {
-        		me.hide();
-        	}
-        });
-        
-        me.buttons = [ok, cancel];
-        
 
-        me.callParent(arguments);
-    }
+			height : 200,
+			width : 400,
 
-});
+			plugins : [Ext.create('Ext.grid.plugin.CellEditing', {
+				clicksToEdit : 1
+			})],
+
+			dockedItems : [{
+				xtype : 'pagingtoolbar',
+				store : 'MetricStore',
+				dock : 'bottom',
+				displayInfo : true
+			}],
+		});
+
+		me.items = [grid];
+
+		var _store = grid.getStore();
+		//var _selModel = grid.getSelectionModel();
+
+		var ok = Ext.create('Ext.Button', {
+			id : 'update_metrics_button',
+			text : '生成Metric',
+		});
+
+		var cancel = Ext.create('Ext.Button', {
+			text : '取消',
+			handler : function() {
+				me.hide();
+			}
+		});
+
+		me.buttons = [ok, cancel];
+
+		me.callParent(arguments);
+	}
+}); 
--- src/main/webapp/app/view/ModelView/MetricWindow.js
+++ src/main/webapp/app/view/ModelView/MetricWindow.js
@@ -1,72 +1,75 @@
 Ext.define('PaaSMonitor.view.ModelView.MetricWindow', {
-    extend: 'Ext.window.Window',
-    alias: 'widget.showmetric',
-    
-    requires: [
-       'PaaSMonitor.store.CustomedMetricStore'
-    ],
-    
-    layout: 'fit',
-    height: 250,
-    width: 400,
-    title: '显示该资源的监测数据',
-    
-    closeAction: 'hide',
-    
-    //除窗口外背景全部变为暗色，然后其他区域不能操作
-    modal: false,
+	extend : 'Ext.window.Window',
+	alias : 'widget.showmetric',
 
-    initComponent: function() {
-        var me = this;
+	requires : ['PaaSMonitor.store.CustomedMetricStore'],
 
-        //在此处添加需要在window中使用的组件，如grid  
-        
-        var grid = Ext.create('Ext.grid.Panel', {
-        	id: 'metricviewgrid',
-        	store: 'CustomedMetricStore',
-			columns:[{
-				header: '名称',  
-				dataIndex: 'templateName' 
-				},{
-				header: '间隔/ms', dataIndex: 'interval', 
-				format: '0,000',
-				flex: 1
-				},{
-				header: '单位', dataIndex: 'templateUnits' 
-				},{
-				header: '显示监测历史数据',
-				xtype: 'actioncolumn',
-				id: 'showdata',
-				items: [{
-					
-					icon: 'images/showchart.png',
-					alertText: '显示监测历史数据',
-					tooltip: '点击显示监测历史数据'
+	layout : 'fit',
+	height : 250,
+	width : 400,
+	title : '显示该资源的监测数据',
+
+	closeAction : 'hide',
+
+	//除窗口外背景全部变为暗色，然后其他区域不能操作
+	modal : false,
+
+	initComponent : function() {
+		var me = this;
+
+		//在此处添加需要在window中使用的组件，如grid
+
+		var grid = Ext.create('Ext.grid.Panel', {
+			id : 'metricviewgrid',
+			store : 'CustomedMetricStore',
+			columns : [{
+				header : '名称',
+				dataIndex : 'templateName'
+			}, {
+				header : '间隔/min',
+				dataIndex : 'interval',
+				format : '0,000',
+				flex : 1,
+				renderer : function(value, cellmeta, record, rowIndex, columnIndex, store) {
+					var readable = value /(1000*60);
+					return readable;
+				}
+			}, {
+				header : '单位',
+				dataIndex : 'templateUnits'
+			}, {
+				header : '显示监测历史数据',
+				xtype : 'actioncolumn',
+				id : 'showdata',
+				items : [{
+
+					icon : 'images/showchart.png',
+					alertText : '显示监测历史数据',
+					tooltip : '点击显示监测历史数据'
 				}]
 			}],
-			selType: 'rowmodel',
-			height: 200,
-			width: 400,
-			dockedItems: [{
-				xtype: 'pagingtoolbar',
-				store: 'CustomedMetricStore',
-				dock: 'bottom',
-				displayInfo: true
-    		}],
-        });
-        
-        me.items = [grid];
-        
-        var close = Ext.create('Ext.Button', {
-        	text: '关闭', 
-        	handler: function() {
-        		me.hide();
-        	}
-        });
-        
-        me.buttons = [close];
+			selType : 'rowmodel',
+			height : 200,
+			width : 400,
+			dockedItems : [{
+				xtype : 'pagingtoolbar',
+				store : 'CustomedMetricStore',
+				dock : 'bottom',
+				displayInfo : true
+			}],
+		});
 
-        me.callParent(arguments);
-    }
+		me.items = [grid];
 
-});
+		var close = Ext.create('Ext.Button', {
+			text : '关闭',
+			handler : function() {
+				me.hide();
+			}
+		});
+
+		me.buttons = [close];
+
+		me.callParent(arguments);
+	}
+}); 
